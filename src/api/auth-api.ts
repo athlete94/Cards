e2046b009a7af9630ac17f228cdbb7d87fd5009a
Asont <a.druzhko@gmail.com>
2022-06-25 21:57:02 +0300

    create authApi

---
+++ src/api/auth-api.ts
@@ -0,0 +1,30 @@
+import axios from "axios";
+import {FormLoginType} from "../components/Login/Login";
+
+export const instance = axios.create({
+    baseURL:'http://localhost:7542/2.0/',
+    withCredentials: true,
+})
+
+export const authApi = {
+    login(payload:FormLoginType){
+        return instance.post<ResponseUserDataLogin>('auth/login', payload)
+    },
+
+}
+
+
+type ResponseUserDataLogin = {
+    _id: string;
+    email: string;
+    name: string;
+    avatar?: string;
+    publicCardPacksCount: number;
+    created: Date;
+    updated: Date;
+    isAdmin: boolean;
+    verified: boolean; // подтвердил ли почту
+    rememberMe: boolean;
+    error?: string;
+}
+
