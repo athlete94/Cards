2fd96103970b490e9065e01841c9b8fffad38fb8
Asont <a.druzhko@gmail.com>
2022-06-25 22:11:37 +0300

    add login

---
+++ src/components/Login/Login.tsx
@@ -0,0 +1,90 @@
+import { useFormik } from 'formik';
+import React from 'react';
+import style from './Login.module.css'
+import {Button, Checkbox, FormControl, FormControlLabel, FormGroup, TextField} from "@material-ui/core";
+import { useTypedDispatch} from "../../redux/store";
+import {loginTC} from "../../redux/authReducer";
+
+export type FormLoginType = {
+    email:string,
+    password:string,
+    rememberMe:boolean,
+}
+
+const Login = () => {
+
+    const dispatch = useTypedDispatch()
+
+    const formik = useFormik({
+        initialValues: {
+            email: "",
+            password: "",
+            rememberMe: false,
+        },
+        validate: (values) => {
+            const errors: any = {};
+            if (!values.email) {
+                errors.email = "Required";
+            } else if (
+                !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+            ) {
+                errors.email = "Invalid email address";
+            }
+            if (!values.password) {
+                errors.password = "Required";
+            } else if (values.password.length < 8) {
+                errors.password = "Must be 8 characters or less";
+            }
+            return errors;
+        },
+        onSubmit: (values) => {
+        dispatch(loginTC(values));
+             formik.resetForm();
+        },
+    });
+
+    return (
+        <div className={style.projectBlock}>
+            <div className={style.container}>
+                <h3>It-incubator</h3>
+                <h3>Sign in</h3>
+                <form onSubmit={formik.handleSubmit}>
+                    <FormControl>
+                        <FormGroup>
+                            <TextField
+                                type="email"
+                                label="Email"
+                                margin="normal"
+                                variant="standard"
+                                {...formik.getFieldProps("email")}
+                            />
+                            {formik.touched.email && formik.errors.email ? (
+                                <div style={{ color: "red", fontStyle:"10px" }}>{formik.errors.email}</div>
+                            ) : null}
+                            <TextField
+                                id="standard-password-input"
+                                label="Password"
+                                type="password"
+                                autoComplete="current-password"
+                                variant="standard"
+                                {...formik.getFieldProps("password")}
+                            />
+                            {formik.touched.password && formik.errors.password ? (
+                                <div style={{ color: "red" }}>{formik.errors.password}</div>
+                            ) : null}
+                            <FormControlLabel
+                                label={"Remember me"}
+                                control={<Checkbox  {...formik.getFieldProps("rememberMe")} />}
+                            />
+                            <Button size={'small'} type={"submit"} variant={"contained"} color={"inherit"}>
+                                Login
+                            </Button>
+                        </FormGroup>
+                    </FormControl>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
